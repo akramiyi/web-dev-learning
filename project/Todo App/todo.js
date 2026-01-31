a33c1797c9d2297976a001d8c93836ead40ab858
Akram Ansari <meakramiyi@gmail.com>
2026-01-31 20:55:36 +0530

    push all

---
+++ project/Todo App/todo.js
@@ -0,0 +1,43 @@
+let todoList = [
+     {
+        item:'EKRAM ',
+        dueDate : '20/1/2026'
+    } , 
+     { 
+        item:'AKRAM',
+        dueDate : '20/1/2026'
+    }
+    ];
+
+displayItems();
+
+function addTodo (){
+    let inputElement = document.querySelector('#todo-input');
+    let dateElement = document.querySelector('#todo-date');
+    let todoItem = inputElement.value;
+    let todoDate = dateElement.value;
+    todoList.push({item:todoItem,dueDate : todoDate});
+    inputElement.value = '';
+    dateElement.value = '';
+
+    displayItems();
+}
+function displayItems(){
+    let containerElement = document.querySelector
+        ('.todo-contanier');
+        let newHtml = ' ';
+        for (let i = 0 ; i  < todoList.length ; i++){  
+            // let item = todoList[i].item;
+            // let dueDate = todoList[i].dueDate;
+            let {item , dueDate} = todoList[i];
+           newHtml += ` 
+           
+           <span>${item}</span>
+          <span>${dueDate}</span>
+        <button  class="btn-delete"  onclick="todoList.splice(${i}, 1);
+         displayItems();">Delete</button>
+    
+    ` ;
+        }
+        containerElement.innerHTML = newHtml ;
+    }
